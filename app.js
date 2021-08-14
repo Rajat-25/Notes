288f716f013c9424df4687f9076c59c3334b3f22
Rajat <rajatkr125@gmail.com>
2021-08-14 14:40:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const chalk=require('chalk')
+const yargs =require('yargs')
+const notes =require('./notes')
+//yargs.version('1.1.0')
+yargs.command({
+  command:'add',
+  describe:'Add a new note',
+  builder:{
+    title:{
+      describe:'Note title',
+      demandOption:true,
+      type:'string'
+    },
+    body:{
+      describe:'Note bode',
+      demandOption:true,
+      type:'string'
+    }
+  },
+  handler:function(argv){
+    notes.addNote(argv.title,argv.body)
+
+  }
+})
+yargs.command({
+  command:'remove',
+  describe:'Remove a note',
+  builder:{
+    title:{
+      describe:'Note title',
+      demandOption:true,
+      type:'string'
+    }
+  },
+  handler:function(argv){
+    notes.removeNote(argv.title)
+  }
+})
+yargs.command({
+  command:'list',
+  describe:'List a note',
+  handler:function(){
+    console.log('Listing the note')
+  }
+})
+yargs.command({
+  command:'read',
+  describe:'Read a note',
+  handler:function(){
+    console.log('Reading the note')
+  }
+})
+yargs.parse()
